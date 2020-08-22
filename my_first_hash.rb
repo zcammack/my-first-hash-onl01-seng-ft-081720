1ce4726ef8988bb4bbd222121537f1dbf57d3758
Zackary Cammack <zack.cammack@gmail.com>
2020-08-22 14:12:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,7 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
