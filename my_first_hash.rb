64ca6226bc8cd3e07c490cc74d74bcf09e151359
Zackary Cammack <zack.cammack@gmail.com>
2020-08-22 14:11:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  your_hash = {"name" => "Zackary Cammack", "age" => "26", "profession" => "Programmer"}
 end
 
 
